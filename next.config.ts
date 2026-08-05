3026798ac3d017bb87adad204c31aa76d9cf6f75
Hussein Duvigneau <hussein@notmybase.com>
2026-08-05 13:41:46 +0200

    remove console.log from production

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,14 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
 };
 
+// Safely apply the compiler config dynamically only for production builds
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.compiler = {
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? { exclude: ["error", "warn"] }
+        : false,
+  };
+}
+
 export default nextConfig;
